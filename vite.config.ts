374b450df979c9cf5aa9ec5ef7c2115a724adf5f
Hugh He <plushugh@gmail.com>
2023-02-28 10:03:36 +0800

    (deployment) remove uploading sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,9 @@
-import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
-	process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 	return {
-		plugins: [
-			sveltekit(),
-			sentryVitePlugin({
-				org: process.env.SENTRY_ORG,
-				project: process.env.SENTRY_PROJECT,
-
-				// Specify the directory containing build artifacts
-				include: './build',
-
-				// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-				// and need `project:releases` and `org:read` scopes
-				authToken: process.env.SENTRY_AUTH_TOKEN,
-
-				telemetry: false
-			})
-		],
+		plugins: [sveltekit()],
 		build: {
 			sourcemap: true
 		},
