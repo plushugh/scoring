ea36545d6bf0a34b9d1b73a84924ab0a7b295560
Hugh He <plushugh@gmail.com>
2023-02-28 09:41:52 +0800

    (deployment) fix vite envvars

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,33 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-const config: UserConfig = {
-	plugins: [
-		sveltekit(),
-		sentryVitePlugin({
-			org: process.env.SENTRY_ORG,
-			project: process.env.SENTRY_PROJECT,
+export default defineConfig(({ mode }) => {
+	process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+	return {
+		plugins: [
+			sveltekit(),
+			sentryVitePlugin({
+				org: process.env.SENTRY_ORG,
+				project: process.env.SENTRY_PROJECT,
 
-			// Specify the directory containing build artifacts
-			include: './build',
+				// Specify the directory containing build artifacts
+				include: './build',
 
-			// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-			// and need `project:releases` and `org:read` scopes
-			authToken: process.env.SENTRY_AUTH_TOKEN
-		})
-	],
-	build: {
-		sourcemap: true
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData: '@use "src/variables.scss" as *;'
+				// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+				// and need `project:releases` and `org:read` scopes
+				authToken: process.env.SENTRY_AUTH_TOKEN
+			})
+		],
+		build: {
+			sourcemap: true
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "src/variables.scss" as *;'
+				}
 			}
 		}
-	}
-};
-
-export default config;
+	};
+});
